93a7487e630f4df0d533768edcd89a0e534cc7b6
ladlad5 <gilad.stessel@gmail.com>
2019-02-11 09:14:01 -0500

    started work on button mapping

--- src/main/java/org/ligerbots/robot/OI.java
+++ src/main/java/org/ligerbots/robot/OI.java
@@ -7,8 +7,15 @@
 
 package org.ligerbots.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import org.ligerbots.robot.Commands.DriveToVisionTarget;
+import org.ligerbots.robot.Commands.GrabberKickerToggleCommand;
+import org.ligerbots.robot.Commands.GrabberToggleCommand;
+import org.ligerbots.robot.Commands.IntakeToggleCommand;
+
 
 /**
  * Add your docs here.
@@ -16,10 +23,30 @@ import edu.wpi.first.wpilibj.GenericHID.Hand;
 public class OI {
 
     XboxController xbox;
+    Joystick farm;
 
 
     public OI () {
         xbox = new XboxController(0);
+        farm = new Joystick(1);
+
+        JoystickButton xBoxA = new JoystickButton(xbox, 1);
+        xBoxA.whenPressed(new GrabberKickerToggleCommand());
+
+        JoystickButton xBoxB = new JoystickButton(xbox, 2);
+        xBoxB.whenPressed(new DriveToVisionTarget());
+
+        JoystickButton xBoxBumperRight = new JoystickButton(xbox, 6);
+        //xBoxBumperRight.whenPressed(new FieldCentricToggleCommand()); TODO make field centric toggle command
+
+        JoystickButton xBoxBumperLeft = new JoystickButton(xbox, 5);
+        xBoxBumperLeft.whenPressed(new IntakeToggleCommand());
+
+        JoystickButton xBoxRightJoystick = new JoystickButton(xbox, 10);
+        xBoxRightJoystick.whenPressed(new GrabberToggleCommand(true));
+
+        JoystickButton xBoxLeftJoystick = new JoystickButton(xbox, 9);
+        xBoxLeftJoystick.whenPressed(new GrabberToggleCommand(false));
     }
 
     public double getThrottle () {
